3fc6c1aa3d674a03595bc0cd4150170c330f65c1
KingSolomon1954 <KingSolomon1954@gmail.com>
2020-04-09 16:52:37 -0700

    Start color

---
+++ shellLib/libColor.bash
@@ -0,0 +1,11 @@
+
+
+# See git clone --depth=1 https://github.com/Bash-it/bash-it.git
+# then bash-it/themes/colors.theme.bash
+
+function __color_rgb {
+  r=$1 && g=$2 && b=$3
+  [[ r == g && g == b ]] && echo $(( $r / 11 + 232 )) && return # gray range above 232
+  echo "8;5;$(( ($r * 36  + $b * 6 + $g) / 51 + 16 ))"
+}
+
