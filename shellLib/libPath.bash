11129ef8560eddd8b687c45b838f4052d897f224
KingSolomon <KingSolomon1954@gmail.com>
2020-08-22 22:39:47 -0700

    Upgrad libPath functions.

--- shellLib/libPath.bash
+++ shellLib/libPath.bash
@@ -1,12 +1,31 @@
-# Functions to manipulate shell PATH like variables
-# with entries separated by ":" such as MANPATH and
-# PATH itself.
+# Functions to manipulate shell environment PATH like
+# variables with entries separated by ":" such as 
+# MANPATH and PATH itself.
+#
+# Normally these variables contain directory paths
+# but they don't have to. Between each path element
+# is a separator character, normally a colon ":"
+# but this can be changed by setting evpSep.
+#
+# At the time a directory is added to a PATH variable,
+# the existence of the directory can be tested for
+# and skipped if not found. See envp.setDirMustExist().
+# 
+# envp.contains(PATH, dir)
+# envp.append  (PATH, dir)
+# envp.prepend (PATH, dir)
+# envp.delete  (PATH, dir)
+# envp.setSeparator(char)
+# envp.setDirMustExist("true"/"false")
 #
 # Source this file to bring functions into environment.
 #
 # Avoid double inclusion
-[[ -v libPathImported ]] && return 0
-libPathImported=1
+[[ -v libEnvpImported ]] && return 0
+libEnvpImported=1
+
+envpSep=":"
+_envpDirMustExist=true
 
 # -----------------------------------------------------------
 #
@@ -18,64 +37,62 @@ libPathImported=1
 # expecting ":" separating individual elements.
 #
 # Example:
-#     if isInPath /usr/bin PATH; then
+#     if envp.contains PATH "/usr/bin"; then
 #         echo "Yes in PATH"
 #     fi
 #
-isInPath()
+envp.contains()
 {
-    [[ $# -lt 2 ]]   && return 2 # return error, missing args
-    [[ ${#1} == 0 ]] && return 2 # return error, empty 1st arg
-    [[ ${#2} == 0 ]] && return 2 # return error, empty 2nd arg
-    local -nr v=$2
-    case ":${v}:" in
-        *":$1:"*) return 0;;  # Found
-        *)        return 1;;  # Not found
+    if ! _envp.checkArgs $1 "$2"; then return 2; fi
+    local -rn v=$1
+    case "${envpSep}${v}${envpSep}" in
+        *"${envpSep}${2}${envpSep}"*) return 0;;  # Found
+        *) return 1;;  # Not found
     esac
 }
 
 # -----------------------------------------------------------
 #
-# Append $1 to $2 provided $1 is not already in the
-# given path variable. $2 is the variable name of a path 
+# Append $2 to $1 provided $2 is not already in the
+# given path variable. $1 is the variable name of a path 
 # style variable with ":" separating individual elements.
+# Optionally skips adding the fragment to the path if
+# it doesn't exist on the file space - see 
+# envp.setDirMustExist()
 #
 # Example:
-#     appendToPath $HOME/man MANPATH
+#     envp.append MANPATH $HOME/man
 #
-appendToPath ()
+envp.append()
 {
-    if isInPath "${1}" ${2}; then
-        return 0
-    fi
-    [[ $# -lt 2 ]]   && return 2 # return error, missing args
-    [[ ${#1} == 0 ]] && return 2 # return error, empty 1st arg
-    [[ ${#2} == 0 ]] && return 2 # return error, empty 2nd arg
-    local -n v=$2
-    v="${v}:$1"
-    colonTrimPath $2
+    if ! _envp.checkArgs $1 $2; then return 2; fi
+    if envp.contains $1 "$2"; then return 0; fi
+    [[ "${_envpDirMustExist}" = true ]] && [[ ! -d "$2" ]] && return 0
+    local -n v=$1
+    v="${v}${envpSep}$2"
+    _envp.colonTrimPath $1
 }
 
 # -----------------------------------------------------------
 #
-# Prepend $1 to $2 provided # $1 is not already in the
-# given path variable. $2 is a path style variable 
+# Prepend $2 to $1 provided # $1 is not already in the
+# given path variable. $1 is a path style variable 
 # with ":" separating individual elements.
+# Optionally skips adding the fragment to the path if
+# it doesn't exist on the file space - see 
+# envp.setDirMustExist()
 #
 # Example:
-#     prependToPath $HOME/man MANPATH
+#     envp.prepend MANPATH $HOME/man
 #
-prependToPath ()
+envp.prepend()
 {
-    if isInPath "$1" ${2}; then
-        return 0
-    fi
-    [[ $# -lt 2 ]]   && return 2 # return error, missing args
-    [[ ${#1} == 0 ]] && return 2 # return error, empty 1st arg
-    [[ ${#2} == 0 ]] && return 2 # return error, empty 2nd arg
-    local -n v=$2
-    v="$1:${v}"
-    colonTrimPath ${2}
+    if ! _envp.checkArgs $1 $2; then return 2; fi
+    if envp.contains $1 "$2"; then return 0; fi
+    [[ "${_envpDirMustExist}" = true ]] && [[ ! -d "$2" ]] && return 0
+    local -n v=$1
+    v="$2${envpSep}${v}"
+    _envp.colonTrimPath $1
 }
 
 # -----------------------------------------------------------
@@ -84,51 +101,101 @@ prependToPath ()
 # variable with ":" separating individual elements.
 #
 # Example:
-#     deleteFromPath $HOME/man MANPATH
+#     envp.delete MANPATH "$HOME/man"
 #
-deleteFromPath ()
+envp.delete()
 {
-    [[ $# -lt 2 ]]   && return 2 # return error, missing args
-    [[ ${#1} == 0 ]] && return 2 # return error, empty 1st arg
-    [[ ${#2} == 0 ]] && return 2 # return error, empty 2nd arg
-    local -n v=$2
-    v="${v//${1}/}"  # Sub it out
-    colonTrimPath ${2}
+    if ! _envp.checkArgs $1 $2; then return 2; fi
+    local -n v=$1
+    v="${v//${2}/}"  # Sub it out
+    _envp.colonTrimPath $1
 }
 
 # -----------------------------------------------------------
 #
-# Remove 1st element from $1. $1 is the name of a path style 
-# variable with ":" separating individual elements.
-# If $1 is empty, an empty string is returned along with a 
-# an error status, othwerwise the first element of $1 up to
-# the first ":" is removed from $1 and returned along with
-# a status of success.
+# Delete 1st element from $1 where $1 is the name of a
+# path style variable with ":" separating individual 
+# elements. Returns 0 (true) if something was deleted,
+# 1 (false) if nothing was deleted, 2 if an error.
 #
 # Example:
-#     removeFirstFromPath $HOME/man MANPATH
+#     envp.deleteFirst MANPATH
 #
-removeFirstFromPath ()
+envp.deleteFirst()
 {
-    [[ $# -lt 1 ]]   && return 2 # return error, missing args
-    [[ ${#1} == 0 ]] && return 2 # return error, empty 1st arg
+    [[ $# -lt 1 ]]   && return 2 # return error, missing arg
+    [[ ${#1} == 0 ]] && return 2 # return error, empty arg
     local -n v=$1
-    local result
-    if isEmptyR $1; then
-        echo ""
-        return 1
+    [[ ${#v} == 0 ]] && return 1 # contains an empty string
+    v=${v}${envpSep}     # add sentinel in case single frag
+    pattern="*${envpSep}"
+    v=${v#${pattern}}    # delete first including separator
+    v=${v%${envpSep}}    # remove sentinel
+    return 0
+}
+
+# -----------------------------------------------------------
+#
+# Delete last element from $1 where $1 is the name of a
+# path style variable with ":" separating individual 
+# elements. Returns 0 (true) if something was deleted,
+# 1 (false) if nothing was deleted, 2 if an error.
+#
+# Example:
+#     envp.deleteLast MANPATH
+#
+envp.deleteLast()
+{
+    [[ $# -lt 1 ]]   && return 2 # return error, missing arg
+    [[ ${#1} == 0 ]] && return 2 # return error, empty arg
+    local -n v=$1
+    [[ ${#v} == 0 ]] && return 1 # contains an empty string
+    v=${envpSep}${v}     # add sentinel in case single frag
+    pattern="${envpSep}*"
+    v=${v%${pattern}}    # delete first including separator
+    v=${v#${envpSep}}    # remove sentinel
+    return 0
+}
+
+# -----------------------------------------------------------
+#
+# Supply $1 = "true" or "false"
+#
+# When appending or prepending a directory fragment,
+# skip adding it if it doesn't exist on the file space.
+# If you are working with PATH style environment variables 
+# that are not directory fragments, (e.g. FIGNORE), then
+# you should set this to false for the scope of that call.
+
+envp.setDirMustExist()
+{
+    if [ "$1" = "true" ]; then
+        _envpDirMustExist=true
+    else
+        _envpDirMustExist=false
     fi
-    colonTrimPath ${2}
+}
+
+# -----------------------------------------------------------
+#
+# Helper function
+#
+_envp.checkArgs()
+{
+    [[ $# -ne 2 ]]   && return 2 # return error, missing args
+    [[ ${#1} == 0 ]] && return 2 # return error, empty 1st arg
+    [[ ${#2} == 0 ]] && return 2 # return error, empty 2nd arg
+    return 0  # args are good
 }
 
 # -----------------------------------------------------------
 
-colonTrimPath ()
+_envp.colonTrimPath ()
 {
-    local -n d=${1}
-    d=${d//::/:}  # Clean up double colons
-    d=${d#:}      # Clean up first colon
-    d=${d%:}      # Clean up trailing colon
+    local -n v=${1}
+    v=${v//${envpSep}${envpSep}/${envpSep}}  # Clean up double colons
+    v=${v#${envpSep}}      # Clean up first colon
+    v=${v%${envpSep}}      # Clean up trailing colon
 }
 
 # -----------------------------------------------------------
--- shellLib/tests/libPathTest.bash
+++ shellLib/tests/libPathTest.bash
@@ -6,234 +6,256 @@ import libPath.bash
 
 # -----------------------------------------------------------
 
-test_isInPath()
+test_envp.contains()
 {
     # test insufficient args - no args
-    assert_fail "isInPath"
+    assert_fail "envp.contains"
 
     # test insufficient args - 1 arg
-    assert_fail "isInPath '/usr/dinosaur'"
+    assert_fail "envp.contains '/usr/dinosaur'"
 
     # test two empty args
-    assert_fail "isInPath '' ''"
+    assert_fail "envp.contains '' ''"
 
     # test empty args
-    assert_fail "isInPath '/usr/dinosaur' ''"
+    assert_fail "envp.contains '/usr/dinosaur' ''"
 
     # test empty args
-    assert_fail "isInPath '' '/usr/dinosaur'"
+    assert_fail "envp.contains '' '/usr/dinosaur'"
     
     # test unset var
-    assert_fail "isInPath '/usr/dinosaur' 'SOME_UNSETVAR'"
+    assert_fail "envp.contains 'SOME_UNSETVAR' '/usr/dinosaur'"
 
     # test null var
     SOME_EMPTY_VAR=
-    assert_fail "isInPath '/usr/dinosaur' 'SOME_EMPTY_VAR'"
+    assert_fail "envp.contains 'SOME_EMPTY_VAR' '/usr/dinosaur'"
 
     # test null var
     SOME_EMPTY_VAR=
-    assert_fail "isInPath '/usr/dinosaur' 'SOME_EMPTY_VAR'"
-
-    # test null var
-    SOME_EMPTY_VAR=
-    assert_fail "isInPath '/usr/dinosaur' 'SOME_EMPTY_VAR'"
+    assert_fail "envp.contains 'SOME_EMPTY_VAR' '/usr/dinosaur'"
 
     # test empty var
     SOME_EMPTY_VAR=""
-    assert_fail "isInPath '/usr/dinosaur' 'SOME_EMPTY_VAR'"
+    assert_fail "envp.contains 'SOME_EMPTY_VAR' '/usr/dinosaur'"
 
     # test simple content - no match
     PATH_VAR="/usr/bird"
-    assert_fail "isInPath 'bird' 'PATH_VAR'"
+    assert_fail "envp.contains 'PATH_VAR' 'bird'"
 
     # test simple content - matching content
     PATH_VAR="/usr/bird"
-    assert "isInPath '/usr/bird' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/usr/bird'"
 
     # test simple content trailing ":" - matching content
     PATH_VAR="/usr/bird:"
-    assert "isInPath '/usr/bird' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/usr/bird'"
     
     # test simple content leading ":" - matching content
     PATH_VAR=":/usr/bird"
-    assert "isInPath '/usr/bird' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/usr/bird'"
 
     # test content - matching content at front
     PATH_VAR="/usr/bird:/front_schmutz:/end_schmutz"
-    assert "isInPath '/usr/bird' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/usr/bird'"
 
     # test content - matching content
     PATH_VAR="/front_schmutz:/usr/bird:/end_schmutz"
-    assert "isInPath '/usr/bird' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/usr/bird'"
 
     # test content - matching content at end
     PATH_VAR="/front_schmutz:/end_schmutz/:/usr/bird"
-    assert "isInPath '/usr/bird' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/usr/bird'"
 
     # test content - spaces in content
     PATH_VAR="/front schmutz:/end schmutz/:/usr/bird"
-    assert "isInPath '/end schmutz/' 'PATH_VAR'"
+    assert "envp.contains 'PATH_VAR' '/end schmutz/'"
 
     # test content - spaces in content
     PATH_VAR="/front schmutz:/end schmutz/:/usr/bird"
-    assert_fail "isInPath 'schmutz' 'PATH_VAR'"
+    assert_fail "envp.contains 'PATH_VAR' 'schmutz'"
 }
 
 # -----------------------------------------------------------
 
-test_appendToPath()
+test_envp.checkArgs()
 {
     # test insufficient args - no args
-    assert_fail "appendToPath"
+    assert_fail "_envp.checkArgs"
 
     # test insufficient args - 1 arg
-    assert_fail "appendToPath '/usr/dinosaur'"
+    assert_fail "_envp.checkArgs ''"
+
+    # test 2 emtpy args
+    assert_fail "_envp.checkArgs '' ''"
+
+    # test 1 emtpy arg
+    assert_fail "_envp.checkArgs '' 'here i am'"
+
+    # test 1 emtpy arg
+    assert_fail "_envp.checkArgs 'here i am' ''"
+
+    # test good args
+    assert "_envp.checkArgs 'PATH' 'some file'"
+}
+
+# -----------------------------------------------------------
+
+test_envp.append()
+{
+    envp.setDirMustExist "false"
+    # test insufficient args - no args
+    assert_fail "envp.append"
+
+    # test insufficient args - 1 arg
+    assert_fail "envp.append '/usr/dinosaur'"
 
     # test two empty args
-    assert_fail "appendToPath '' ''"
+    assert_fail "envp.append '' ''"
 
     # test empty args
-    assert_fail "appendToPath '/usr/dinosaur' ''"
+    assert_fail "envp.append '/usr/dinosaur' ''"
 
     # test empty args
-    assert_fail "appendToPath '' '/usr/dinosaur'"
+    assert_fail "envp.append '' '/usr/dinosaur'"
     
     # test appending to an unset var
     unset SOME_UNSETVAR
-    appendToPath /usr/dinosaur SOME_UNSETVAR
+    envp.append SOME_UNSETVAR /usr/dinosaur 
     assert_equals "/usr/dinosaur" "${SOME_UNSETVAR}"
     unset SOME_UNSETVAR
 
     # test appending to existing var already with content
     SOME_PATHVAR="/usr/dinosaur"
-    appendToPath /usr/phylum SOME_PATHVAR
+    envp.append SOME_PATHVAR /usr/phylum
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 
     # test appending to existing var with trailing colon
     SOME_PATHVAR="/usr/dinosaur:"
-    appendToPath /usr/phylum SOME_PATHVAR
+    envp.append SOME_PATHVAR /usr/phylum 
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
     
     # test appending to existing var with duplicate front content
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum"
-    appendToPath /usr/dinosaur SOME_PATHVAR
+    envp.append SOME_PATHVAR /usr/dinosaur
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 
     # test appending to existing var with duplicate end content
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum"
-    appendToPath /usr/phylum SOME_PATHVAR
+    envp.append SOME_PATHVAR /usr/phylum
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 
     # test appending to existing var with extra ":" in content
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum"
-    appendToPath ":/usr/games:" SOME_PATHVAR
+    envp.append SOME_PATHVAR ":/usr/games:"
     assert_equals "/usr/dinosaur:/usr/phylum:/usr/games" "${SOME_PATHVAR}"
 }
 
 # -----------------------------------------------------------
 
-test_prependToPath()
+test_env.prepend()
 {
+    envp.setDirMustExist "false"
+
     # test insufficient args - no args
-    assert_fail "prependToPath"
+    assert_fail "envp.prepend"
 
     # test insufficient args - 1 arg
-    assert_fail "prependToPath '/usr/dinosaur'"
+    assert_fail "envp.prepend '/usr/dinosaur'"
 
     # test two empty args
-    assert_fail "prependToPath '' ''"
+    assert_fail "envp.prepend '' ''"
 
     # test empty args
-    assert_fail "prependToPath '/usr/dinosaur' ''"
+    assert_fail "envp.prepend '/usr/dinosaur' ''"
 
     # test empty args
-    assert_fail "prependToPath '' '/usr/dinosaur'"
-    
+    assert_fail "envp.prepend '' '/usr/dinosaur'"
+
     # test prepending to an unset var
     unset SOME_UNSETVAR
-    prependToPath /usr/dinosaur SOME_UNSETVAR
+    envp.prepend SOME_UNSETVAR /usr/dinosaur
     assert_equals "/usr/dinosaur" "${SOME_UNSETVAR}"
     unset SOME_UNSETVAR
 
     # test prepending to existing var already with content
     SOME_PATHVAR="/usr/dinosaur"
-    prependToPath /usr/phylum SOME_PATHVAR
+    envp.prepend SOME_PATHVAR /usr/phylum
     assert_equals "/usr/phylum:/usr/dinosaur" "${SOME_PATHVAR}"
 
     # test prepending to existing var with trailing colon
     SOME_PATHVAR="/usr/dinosaur:"
-    prependToPath /usr/phylum SOME_PATHVAR
+    envp.prepend SOME_PATHVAR /usr/phylum
     assert_equals "/usr/phylum:/usr/dinosaur" "${SOME_PATHVAR}"
     
     # test prepending to existing var with duplicate front content
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum"
-    prependToPath /usr/dinosaur SOME_PATHVAR
+    envp.prepend  SOME_PATHVAR /usr/dinosaur
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 
     # test prepending to existing var with duplicate end content
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum"
-    prependToPath /usr/phylum SOME_PATHVAR
+    envp.prepend SOME_PATHVAR /usr/phylum
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 
     # test prepending to existing var with extra ":" in content
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum"
-    prependToPath ":/usr/games:" SOME_PATHVAR
+    envp.prepend SOME_PATHVAR ":/usr/games:"
     assert_equals "/usr/games:/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 }
 
 # -----------------------------------------------------------
 
-test_deleteFromPath()
+test_envp.delete()
 {
     # test insufficient args - no args
-    assert_fail "deleteFromPath"
+    assert_fail "envp.delete"
 
     # test insufficient args - 1 arg
-    assert_fail "deleteFromPath '/usr/dinosaur'"
+    assert_fail "envp.delete '/usr/dinosaur'"
 
     # test two empty args
-    assert_fail "deleteFromPath '' ''"
+    assert_fail "envp.delete '' ''"
 
     # test empty args
-    assert_fail "deleteFromPath '/usr/dinosaur' ''"
+    assert_fail "envp.delete '/usr/dinosaur' ''"
 
     # test empty args
-    assert_fail "deleteFromPath '' '/usr/dinosaur'"
+    assert_fail "envp.delete '' '/usr/dinosaur'"
     
     # test deleting from an unset var
     unset SOME_UNSETVAR
-    deleteFromPath /usr/dinosaur SOME_UNSETVAR
+    envp.delete SOME_UNSETVAR /usr/dinosaur
     assert_equals "" "${SOME_UNSETVAR}"
     unset SOME_UNSETVAR
 
     # test deleting from existing var no matching content
     SOME_PATHVAR="/usr/dinosaur"
-    deleteFromPath /usr/gabage SOME_PATHVAR
+    envp.delete SOME_PATHVAR /usr/gabage
     assert_equals "/usr/dinosaur" "${SOME_PATHVAR}"
 
     # test deleting from existing var matching content
     SOME_PATHVAR="/usr/dinosaur"
-    deleteFromPath /usr/dinosaur SOME_PATHVAR
+    envp.delete SOME_PATHVAR /usr/dinosaur
     assert_equals "" "${SOME_PATHVAR}"
 
     # test deleting from existing var with trailing colon
     SOME_PATHVAR="/usr/dinosaur:"
-    deleteFromPath /usr/dinosaur SOME_PATHVAR
+    envp.delete SOME_PATHVAR /usr/dinosaur
     assert_equals "" "${SOME_PATHVAR}"
     
     # test deleting from var at front
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum:/usr/games"
-    deleteFromPath /usr/dinosaur SOME_PATHVAR
+    envp.delete SOME_PATHVAR /usr/dinosaur
     assert_equals "/usr/phylum:/usr/games" "${SOME_PATHVAR}"
 
     # test deleting from var in middle
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum:/usr/games"
-    deleteFromPath /usr/phylum SOME_PATHVAR
+    envp.delete SOME_PATHVAR /usr/phylum
     assert_equals "/usr/dinosaur:/usr/games" "${SOME_PATHVAR}"
     
     # test deleting from var at end
     SOME_PATHVAR="/usr/dinosaur:/usr/phylum:/usr/games"
-    deleteFromPath /usr/games SOME_PATHVAR
+    envp.delete  SOME_PATHVAR /usr/games
     assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 
     # test deleting from existing var with extra ":" in content
@@ -243,6 +265,70 @@ test_deleteFromPath()
     # SOME_PATHVAR="/usr/dinosaur:/usr/phylum:/usr/games"
     # deleteFromPath ":/usr/phylum:" SOME_PATHVAR
     # assert_equals "/usr/dinosaurs:/usr/games" "${SOME_PATHVAR}"
+}
+
+# -----------------------------------------------------------
+
+test_envp.deleteFirst()
+{
+    # test insufficient args - no args
+    assert_fail "envp.deleteFirst"
+
+    # test empty args
+    assert_fail "envp.deleteFirst ''"
+
+    # test deleting from an unset var
+    unset SOME_UNSETVAR
+    envp.deleteFirst SOME_UNSETVAR
+    assert_equals "" "${SOME_UNSETVAR}"
+    unset SOME_UNSETVAR
+
+    # test deleting from existing but empty var
+    SOME_PATHVAR=""
+    envp.deleteFirst SOME_PATHVAR
+    assert_equals "" "${SOME_PATHVAR}"
+
+    # test deleting first fragment leaving empty string
+    SOME_PATHVAR="/usr/dinosaur"
+    envp.deleteFirst SOME_PATHVAR
+    assert_equals "" "${SOME_PATHVAR}"
+
+    # test deleting first fragment with multiple entries
+    SOME_PATHVAR="/usr/dinosaur:/usr/phylum:/usr/games"
+    envp.deleteFirst SOME_PATHVAR
+    assert_equals "/usr/phylum:/usr/games" "${SOME_PATHVAR}"
+}
+
+# -----------------------------------------------------------
+
+test_envp.deleteLast()
+{
+    # test insufficient args - no args
+    assert_fail "envp.deleteLast"
+
+    # test empty args
+    assert_fail "envp.deleteFirst ''"
+
+    # test deleting from an unset var
+    unset SOME_UNSETVAR
+    envp.deleteLast SOME_UNSETVAR
+    assert_equals "" "${SOME_UNSETVAR}"
+    unset SOME_UNSETVAR
+
+    # test deleting from existing but empty var
+    SOME_PATHVAR=""
+    envp.deleteLast SOME_PATHVAR
+    assert_equals "" "${SOME_PATHVAR}"
+
+    # test deleting last fragment leaving empty string
+    SOME_PATHVAR="/usr/dinosaur"
+    envp.deleteLast SOME_PATHVAR
+    assert_equals "" "${SOME_PATHVAR}"
+
+    # test deleting Last fragment with multiple entries
+    SOME_PATHVAR="/usr/dinosaur:/usr/phylum:/usr/games"
+    envp.deleteLast SOME_PATHVAR
+    assert_equals "/usr/dinosaur:/usr/phylum" "${SOME_PATHVAR}"
 }
 
 # -----------------------------------------------------------
